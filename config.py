40ed8f8427148eda42c25a4bbda79e185c672e10
Luffy Web <drstrangexr1@gmail.com>
2024-08-18 09:41:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("28720464", ""))
-API_HASH = getenv("3c591706418cb4aef1bb2680bf692ee9", "")
-BOT_TOKEN = getenv("7257607789:AAG77SDfrvPztF7aS7SKs-Vmu64QxM3wj88", "")
-OWNER_ID = int(getenv("6174764088", ""))
-MONGODB_CONNECTION_STRING = getenv("mongodb+srv://luffywebbd:@Luffyweb@cluster0.abw5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "")
-LOG_GROUP = int(getenv("-1002209260797", ""))
-FORCESUB = getenv("Luffywebx", "")
+API_ID = "28720464"
+API_HASH = "3c591706418cb4aef1bb2680bf692ee9"
+BOT_TOKEN = "7257607789:AAG77SDfrvPztF7aS7SKs-Vmu64QxM3wj88"
+OWNER_ID = "6174764088"
+MONGODB_CONNECTION_STRING = "mongodb+srv://luffywebbd:@Luffyweb@cluster0.abw5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_GROUP = "-1002209260797"
+FORCESUB = "Luffywebx"
